14041fe5f620cf5f2f9271223920a0beb1ab97b0
angelica-hc <ahuamanc@ulasalle.edu.pe>
2026-05-18 22:43:50 -0500

    2commit

--- problema_03.java
+++ problema_03.java
@@ -1,7 +1,15 @@
 public class problema_03 {
-
-    public static void main(String[] args) {
+    public static int continv(int[] arr, int in, int f) {
+        int conteo = 0;
+        if (in < f) {
+            int medio = (in + f) / 2;
+            conteo += continv(arr, in   , medio);
+            conteo += continv(arr, medio + 1, f);
+            conteo += contm(arr, in , medio, f);
+        }
+        return conteo;
+    }
+    }    public static void main(String[] args) {
         int[] ej = {3, 1, 2}; 
         System.out.println("inversiones: " + continv(ej, 0, ej.length - 1)); 
     }
-}
