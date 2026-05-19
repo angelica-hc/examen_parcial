d73899d9dbe68e7dcaebc13e60ef36748264c388
angelica-hc <ahuamanc@ulasalle.edu.pe>
2026-05-18 20:52:17 -0500

    1.1 commit

--- problema_02.java
+++ problema_02.java
@@ -1,3 +1,11 @@
 public class problema_02 {
-    
+    public static int[] nmayor(int[]arr, int i, int f) {
+
+    }
+    public static void main (String[]as){
+        int [ ]salida={8,3,10,5,7};
+        int r=nmayor(salida,0, salida.length-1);
+        System.out.println("numero mayor es: "+r[1]);
+
+    }
 }
