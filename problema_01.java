f977bcd843c147c52d20d8fb1f497bab11185029
angelica-hc <ahuamanc@ulasalle.edu.pe>
2026-05-18 20:10:16 -0500

    2 commit

--- problema_01.java
+++ problema_01.java
@@ -1,6 +1,12 @@
 public class problema_01{
 public static boolean secue(int [] ar, int n){
-
+if (n==ar.length-1){
+    return true;
+}
+if(ar[n]>ar[n+1]){
+    return false;
+}
+return secue(ar, n+1);
 }
 
     public static void main(String[]as){
