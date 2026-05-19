4a66bd8beef807da9c7b7f52eaf05cf5ddd26062
angelica-hc <ahuamanc@ulasalle.edu.pe>
2026-05-18 20:22:56 -0500

    3 commit

--- problema_01.java
+++ problema_01.java
@@ -10,6 +10,10 @@ return secue(ar, n+1);
 }
 
     public static void main(String[]as){
+        int []arr1={1,2,3,4,5};
+        int []arr2={1,4,2,8};
+        System.out.print("ejemplo1: "+secue(arr1,0));
+        System.out.print("\nejemplo2: "+secue(arr2,0));
 
     }
 }
