1f5681425475e3d4d1a526b5a92fb68c8598b924
angelica-hc <ahuamanc@ulasalle.edu.pe>
2026-05-18 21:15:45 -0500

    2commit

--- problema_02.java
+++ problema_02.java
@@ -1,5 +1,25 @@
 public class problema_02 {
     public static int[] nmayor(int[]arr, int i, int f) {
+                if (i == f) {
+            int[] aux = new int[2];
+            aux[0] = arr[i];
+            aux[1] = -1; 
+            return aux;
+        }
+        int med=(i+f)/2;
+        int[] izq = nmayor(arr, i, med);
+        int[] der = nmayor(arr, med+1, f);
+        int[] res = new int[2];
+        if (izq[0] > der[0]) {
+            res[0]=izq[0];
+            if (izq[1]>der[0]){
+                res[1]=izq[1];
+            }else{
+                res[1]=der[0];
+            }
+        }else{
+            
+        }
 
     }
     public static void main (String[]as){
